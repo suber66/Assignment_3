c103f047c64ddb8868e9fa54f3b92548355affc5
suber66 <dias.achinbaj@gmail.com>
2026-01-19 18:10:17 +0500

    Added SQL code to prepare DataBase

---
+++ src/main/resources/OOP_Project_InsertSQL.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.users(nickname)
+	VALUES ('Sakebay'),('Arubay'),('Miras'),('Dias'),('Maksat');
+INSERT INTO public.games(
+	name, price)
+	VALUES ('Dota 2', 0.00),('Hollow Knight',14.99),('DeadCells', 24.99),('Enter The Gungeon',17.99),('Cyberpunk 2077',59.99);
